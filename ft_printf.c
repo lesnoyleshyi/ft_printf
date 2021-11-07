d3dbecc9cc10954a0c331614011f2790f81fd632
stycho <stycho@student.21-school.ru>
2021-11-07 15:46:38 +0300

    full libft added and complemented with ft_putnbr_base.c, some shitty logic added to ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -12,24 +12,60 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
-	va_list	strings;
+	va_list	ap;
 	int		i;
 
-	va_start(strings, format);
+	va_start(ap, format);
 	while (str[i] != '\0')
 	{
 		if (str[i] == '%')
 		{
 			if (str[i + 1] == '%')
 			{
-				write(1, str[i + 1], 1);
+				write(1, str[++i], 1);
+				i++;
+			}
+			else if (str[i + 1] == 'c')
+			{
+				ft_putchar_fd(va_arg(ap, int), 1);
+				i += 2;
+			}
+			else if (str[i + 1] == 's')
+			{
+				ft_putstr_fd(va_arg(ap, char *), 1);
+				i += 2;
+			}
+			else if (str[i + 1] == 'p')
+			{
+				ft_putnbrbase(va_arg(ap, void *));
+				i += 2;
+			}
+			else if (str[i + 1] == 'd' || str[i + 1] == 'i')
+			{
+				ft_itoa(va_arg(strings, int));
+				i += 2;
+			}
+			else if (str[i + 1] == 'u')
+			{
+				ft_print_unsint(va_arg(strings, unsigned long), 1);
+				i += 2;
+			}
+			else if (str[i + 1] == 'x' || str[i + 1] == 'X')
+			{
+				ft_print_hex(va_arg(strings, unsigned long), 1);
+				i += 2;
+			}
+			else
+			{
+				i += ft_skip_spaces(void);
 			}
 		}
-//		write
+		write(1, str[i++], 1);
 	}
-	va_end(strings);
+	va_end(ap);
 	return (0);
 }
--- test_m.c
+++ test_m.c
@@ -1,8 +1,13 @@
 
 #include <printf.h>
+#include "ft_printf.h"
 
 int	main(void)
 {
-	printf("text before spec \n%	biba");
+	char	*str = "biba";
+	char	*base = "0123456789ABCDEF";
+
+	printf("text %p\n", &str);
+	ft_putnbr_base((unsigned long long)&str, base);
 	return (0);
 }
